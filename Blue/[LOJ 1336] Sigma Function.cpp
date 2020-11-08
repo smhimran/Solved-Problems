af8e358d1c1b0d2dd25db78e9c076d79f41c08de
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2020-11-08 22:31:51 +0600

    November 8, 2020

---
+++ Blue/[LOJ 1336] Sigma Function.cpp
@@ -0,0 +1,154 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+// - - - - - - Data Types - - - - - - //
+
+typedef long int LI;
+typedef long long LL;
+
+// - - - - - - Vectors - - - - - - //
+typedef vector<int> VI;
+typedef vector<LL> VLL;
+typedef vector<string> VS;
+typedef vector<double> VD;
+typedef vector<VI> VVI;
+
+#define scanVI(v, n)        for(int i=0; i<n; i++){ int x; cin >> x; v.PB(x); }
+#define scanVLLI(v, n)      for(int i=0; i<n; i++){ LLI x; cin >> x; v.PB(x); }
+#define scanVS(v, n)        for(int i=0; i<n; i++){ string x; cin >> x; v.PB(x); }
+#define scanVD(v, n)        for(int i=0; i<n; i++){ double x; cin >> x; v.PB(x); }
+
+// - - - - - - Maps - - - - - - //
+typedef map<int, int> MII;
+typedef map<int, string> MIS;
+typedef map<int, char> MIC;
+typedef map<string, int> MSI;
+typedef map<char, int> MCI;
+typedef map<int, VI> MIVI;
+
+// - - - - - - Pairs - - - - - - //
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<char, char> PCC;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<string, char> PSC;
+
+// - - - - - - Sets - - - - - - //
+typedef set<int> SI;
+typedef set<LL> SLL;
+typedef set<string> SS;
+typedef set<char> SC;
+// - - - - - - - - - - - - - - - - - - //
+
+#define PF                  printf
+#define SF                  scanf
+#define PB                  push_back
+#define POP                 pop_back()
+#define PP                  prev_permutation
+#define NP                  next_permutation
+#define MP                  make_pair
+#define CLRN(a, b)          memset(a, b, sizeof(a))
+#define CLR(a)              memset(a, 0, sizeof(a))
+#define ALL(a)              a.begin(), a.end()
+#define ALLN(a, n)          (a, a+n)
+#define BSRCN(a, n, x)      binary_search(ALLN(a, n), x)
+#define BSRC                binary_search
+#define MAX                 10000007
+#define MIN                 -10000007
+#define inf                 int(1e6+9)
+#define PI                  acos(-1)
+#define BR                  PF("\n")
+#define FastIO              ios_base::sync_with_stdio(false)
+#define READ()              freopen("input.txt", "r", stdin)
+#define WRITE()             freopen("output.txt", "w", stdout)
+#define len(a)              a.length()
+#define rsort(a)            sort(a.rbegin(), a.rend())
+#define pvec(v)             for(auto x: v) cout<<x<<" "
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+/*----------------------Graph Moves----------------*/
+int ROW[]={+1,-1,+0,+0};
+int COL[]={+0,+0,+1,-1};
+
+int X[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+int Y[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+
+int KX[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+int KY[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+// ---------------------DEBUG---------------------//
+
+#ifdef HOME
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+//------------------------------------------------//
+
+void fastscan(int &number) 
+{ 
+    bool negative = false; 
+    int c; 
+  
+    number = 0; 
+    c = getchar(); 
+    if (c=='-') 
+    { 
+        negative = true; 
+        c = getchar(); 
+    } 
+    for (; (c>47 && c<58); c=getchar()) 
+        number = number *10 + c - 48; 
+    if (negative) 
+        number *= -1; 
+} 
+
+LL GCD(LL a, LL b) { return b == 0 ? a : GCD(b , a % b); }
+int LCM(int a, int b) { return a * (b/GCD(a, b)); }
+bool CMP(int a, int b) { return a>b; }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - END - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+
+int main()
+{
+    // FastIO;
+    #ifdef HOME
+     clock_t Start=clock();
+     freopen("in.txt", "r", stdin);
+     freopen("out.txt", "w", stdout);
+    #endif
+    
+  	int t, ca=1;
+  	cin>>t;
+  	while (t--) {
+  		LL n;
+  		cin>>n;
+  		LL odds = sqrt(n);
+  		odds += sqrt(n/2);
+  		cout<<"Case "<<ca++<<": "<<(n - odds)<<endl;
+  	}
+
+    END:
+    #ifdef HOME
+     fprintf(stderr, "\n>>Runtime: %.10fs\n", (double) (clock() - Start) / CLOCKS_PER_SEC);
+    #endif
+    return 0;
+}
---
+++ Blue/[UVa 10892] LCM Cardinality.cpp
@@ -0,0 +1,199 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+// - - - - - - Data Types - - - - - - //
+
+typedef long int LI;
+typedef long long LL;
+
+// - - - - - - Vectors - - - - - - //
+typedef vector<int> VI;
+typedef vector<LL> VLL;
+typedef vector<string> VS;
+typedef vector<double> VD;
+typedef vector<VI> VVI;
+
+#define scanVI(v, n)        for(int i=0; i<n; i++){ int x; cin >> x; v.PB(x); }
+#define scanVLLI(v, n)      for(int i=0; i<n; i++){ LLI x; cin >> x; v.PB(x); }
+#define scanVS(v, n)        for(int i=0; i<n; i++){ string x; cin >> x; v.PB(x); }
+#define scanVD(v, n)        for(int i=0; i<n; i++){ double x; cin >> x; v.PB(x); }
+
+// - - - - - - Maps - - - - - - //
+typedef map<int, int> MII;
+typedef map<int, string> MIS;
+typedef map<int, char> MIC;
+typedef map<string, int> MSI;
+typedef map<char, int> MCI;
+typedef map<int, VI> MIVI;
+
+// - - - - - - Pairs - - - - - - //
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<char, char> PCC;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<string, char> PSC;
+
+// - - - - - - Sets - - - - - - //
+typedef set<int> SI;
+typedef set<LL> SLL;
+typedef set<string> SS;
+typedef set<char> SC;
+// - - - - - - - - - - - - - - - - - - //
+
+#define PF                  printf
+#define SF                  scanf
+#define PB                  push_back
+#define POP                 pop_back()
+#define PP                  prev_permutation
+#define NP                  next_permutation
+#define MP                  make_pair
+#define CLRN(a, b)          memset(a, b, sizeof(a))
+#define CLR(a)              memset(a, 0, sizeof(a))
+#define ALL(a)              a.begin(), a.end()
+#define ALLN(a, n)          (a, a+n)
+#define BSRCN(a, n, x)      binary_search(ALLN(a, n), x)
+#define BSRC                binary_search
+#define MAX                 10000007
+#define MIN                 -10000007
+#define inf                 int(1e6+9)
+#define PI                  acos(-1)
+#define BR                  PF("\n")
+#define FastIO              ios_base::sync_with_stdio(false)
+#define READ()              freopen("input.txt", "r", stdin)
+#define WRITE()             freopen("output.txt", "w", stdout)
+#define len(a)              a.length()
+#define rsort(a)            sort(a.rbegin(), a.rend())
+#define pvec(v)             for(auto x: v) cout<<x<<" "
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+/*----------------------Graph Moves----------------*/
+int ROW[]={+1,-1,+0,+0};
+int COL[]={+0,+0,+1,-1};
+
+int X[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+int Y[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+
+int KX[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+int KY[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+// ---------------------DEBUG---------------------//
+
+#ifdef HOME
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+//------------------------------------------------//
+
+void fastscan(int &number) 
+{ 
+    bool negative = false; 
+    int c; 
+  
+    number = 0; 
+    c = getchar(); 
+    if (c=='-') 
+    { 
+        negative = true; 
+        c = getchar(); 
+    } 
+    for (; (c>47 && c<58); c=getchar()) 
+        number = number *10 + c - 48; 
+    if (negative) 
+        number *= -1; 
+} 
+
+LL GCD(LL a, LL b) { return b == 0 ? a : GCD(b , a % b); }
+int LCM(int a, int b) { return a * (b/GCD(a, b)); }
+bool CMP(int a, int b) { return a>b; }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - END - - - - - - - - - - - - - - - - - - - - - - - - - //
+#define LIMIT int(1e7+6)
+bool composite[LIMIT+1];
+vector<LL> prime, v;
+map<LL, LL> times;
+
+void sieve()
+{
+    int i;
+    composite[0]=composite[1]=1;
+    for (i=4; i<=LIMIT; i+=2)
+        composite[i]=1;
+    prime.push_back(2);
+    for (i=3; i*i<=LIMIT; i+=2) {
+        if (!composite[i]) {
+            prime.push_back(i);
+            for (int j=i*i; j<=LIMIT; j+=2*i)
+                composite[j]=1;
+        }
+    }
+    for (; i<=LIMIT; i++)
+        if (!composite[i])
+            prime.push_back(i);
+}
+
+void factorization(LL n) {
+	for (int i=0; i < prime.size() and prime[i] * prime[i] <=n; i++) {
+		if (n % prime[i] == 0) {
+			v.push_back(prime[i]);
+			while (n % prime[i] == 0) {
+				n /= prime[i];
+				times[prime[i]]++;
+			}
+		}
+	}
+
+	if (n > 1) {
+		if (!times[n])
+			v.push_back(n);
+		times[n]++;
+	}
+}
+
+int main()
+{
+    // FastIO;
+    #ifdef HOME
+     clock_t Start=clock();
+     freopen("in.txt", "r", stdin);
+     freopen("out.txt", "w", stdout);
+    #endif
+    
+    sieve();
+  	LL n;
+  	while (cin>>n and n) {
+  		v.clear();
+  		times.clear();
+  		factorization(n);
+  		LL ans = 1;
+  		for (int i=0; i<v.size(); i++) {
+  			ans *= (2LL*times[v[i]] + 1LL);
+  		}
+  		ans++;
+  		ans /= 2;
+      cout<<n<<" "<<ans<<endl;
+  	}
+
+    END:
+    #ifdef HOME
+     fprintf(stderr, "\n>>Runtime: %.10fs\n", (double) (clock() - Start) / CLOCKS_PER_SEC);
+    #endif
+    return 0;
+}
